552bc155d80c151651723a02f700ec121deff9e4
Maciej Michalczyk <maciejmich22@gmail.com>
2023-03-10 18:09:35 +0100

    add header

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 const navMenuBtn = document.querySelector('nav button.nav__menu');
 const navLinksMb = document.querySelector('.nav__links--mobile');
+const navItemsMb = navLinksMb.querySelectorAll('.nav__item');
 
-navMenuBtn.addEventListener('click', () => {
+const removeActiveHandler = () => {
 	navMenuBtn.classList.toggle('active');
 	navLinksMb.classList.toggle('active');
+};
+
+navMenuBtn.addEventListener('click', removeActiveHandler);
+
+document.addEventListener('scroll', () => {
+	navMenuBtn.classList.remove('active');
+	navLinksMb.classList.remove('active');
 });
+
+for (const item of navItemsMb) {
+	item.addEventListener('click', removeActiveHandler);
+}
