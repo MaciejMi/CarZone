bd14d7c01edf3ab0a14ee818239da38c4cd09deb
Maciej Michalczyk <maciejmich22@gmail.com>
2023-04-17 23:26:54 +0200

    add filter in shop site

--- js/main.js
+++ js/main.js
@@ -2,6 +2,27 @@ const navMenuBtn = document.querySelector('nav button.nav__menu');
 const navLinksMb = document.querySelector('.nav__links--mobile');
 const navItemsMb = navLinksMb.querySelectorAll('.nav__item');
 
+const tankCapacityInput = document.querySelector('.tank_capacity_input');
+const tankCapacityOutput = document.querySelector('.tank_capacity_output');
+const minimumCostInput = document.querySelector('.minimum_cost_input');
+const minimumCostOutput = document.querySelector('.minimum_cost_output');
+const maximumCostInput = document.querySelector('.maximum_cost_input');
+const maximumCostOutput = document.querySelector('.maximum_cost_output');
+
+tankCapacityOutput.textContent = 'Tank Capacity: ' + tankCapacityInput.value + ' L';
+minimumCostOutput.textContent = 'Minimum cost: ' + Math.floor(minimumCostInput.value / 1000) * 1000 + ' $';
+maximumCostOutput.textContent = 'Minimum cost: ' + Math.floor(maximumCostInput.value / 1000) * 1000 + ' $';
+
+tankCapacityInput.addEventListener('input', () => {
+	tankCapacityOutput.textContent = 'Tank Capacity: ' + tankCapacityInput.value + ' L';
+});
+minimumCostInput.addEventListener('input', () => {
+	minimumCostOutput.textContent = 'Minimum cost: ' + Math.floor(minimumCostInput.value / 1000) * 1000 + ' $';
+});
+maximumCostInput.addEventListener('input', () => {
+	maximumCostOutput.textContent = 'Minimum cost: ' + Math.floor(maximumCostInput.value / 1000) * 1000 + ' $';
+});
+
 const removeActiveHandler = () => {
 	navMenuBtn.classList.toggle('active');
 	navLinksMb.classList.toggle('active');
