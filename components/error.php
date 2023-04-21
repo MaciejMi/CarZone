440a8d156cededbd0482d3ffefc5593a10a5c43d
Maciej Michalczyk <maciejmich22@gmail.com>
2023-04-21 19:02:36 +0200

    add error page

---
+++ components/error.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/error.css">
+</head>
+
+<body>
+    <h1>Oops!</h1>
+    <img src="../img/logo.png" alt="">
+    <h2>We are currently experiencing technical difficulties. Please stand by</h2>
+    <a href="../index.php" class='btn'>Try going to homepage</a>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    require './components/connection.php';
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 
