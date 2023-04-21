2a65c3f7c7603ab126f6b6aa367d0710cddd2fef
Maciej Michalczyk <maciejmich22@gmail.com>
2023-04-21 12:53:12 +0200

    add edit-offer.html/add-offer.html

---
+++ js/add-file-input.js
@@ -0,0 +1,14 @@
+const numberOfFilesInput = document.getElementById('number-of-files');
+const fileInputContainer = document.querySelector('.file-container');
+
+numberOfFilesInput.addEventListener('input', () => {
+	fileInputContainer.textContent = '';
+	if (numberOfFilesInput.value >= 1 && numberOfFilesInput.value <= 10) {
+		for (let i = 0; i < numberOfFilesInput.value; i++) {
+			const inputFile = document.createElement('input');
+			inputFile.type = 'file';
+			inputFile.name = `file${i}`;
+			fileInputContainer.append(inputFile);
+		}
+	}
+});
